31ba4ac10feb5faa225d553ffd16714245755481
Vidar Magnusson <git@vidarmagnusson.com>
2022-10-29 20:15:42 +0200

    Read file from argument

--- vm_lang/src/main.rs
+++ vm_lang/src/main.rs
@@ -1,19 +1,19 @@
 use vm_lang;
 
 fn main() {
-    vm_lang::run_program(
-        "examples/to_parse.gr",
-        &mut |p| println!("{}", p),
-        &mut || {
-            let mut inp = String::new();
-            std::io::stdin()
-                .read_line(&mut inp)
-                .expect("Failed to read input");
-            match inp.strip_suffix("\n") {
-                Some(s) => String::from(s),
-                None => inp,
-            }
-        },
-    )
+    let args: Vec<String> = std::env::args().collect();
+    let file_path = args.get(1).expect("Expected filepath argument");
+    println!("Got filepath: {}", file_path);
+
+    vm_lang::run_program(file_path, &mut |p| println!("{}", p), &mut || {
+        let mut inp = String::new();
+        std::io::stdin()
+            .read_line(&mut inp)
+            .expect("Failed to read input");
+        match inp.strip_suffix("\n") {
+            Some(s) => String::from(s),
+            None => inp,
+        }
+    })
     .expect("Failure");
 }
